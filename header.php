8cf446328bbf02ff8afacac951e499dcbd57430c
imkent <kent@ideasmaker.net>
2021-05-04 18:21:52 +0800

    update

--- header.php
+++ header.php
@@ -26,23 +26,8 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'imd' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$imd_description = get_bloginfo( 'description', 'display' );
-			if ( $imd_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $imd_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
+		<div class="logo">
+			
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
